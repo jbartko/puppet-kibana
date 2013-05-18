2b48f75a62c4b1e9d7125d5908767cceb19d45fa
John Bartko <jbartko@txstate.edu>
2013-05-18 16:25:47 -0500

    not using bundler module

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,15 +1,29 @@
 class kibana::install {
+  # TODO: parameterize this
+  $path = '/srv/www/kibana'
+
   # TODO: modulefile requires puppetlabs-git
   include git
 
-  vcsrepo { '/srv/www/kibana':
+  package { 'bundler':
+    ensure   => latest,
+    provider => 'gem',
+  }
+
+  # TODO: modulefile requires vcsrepo
+  vcsrepo { $path:
     ensure   => present,
     provider => 'git',
     source   => 'git://github.com/rashidkpc/Kibana.git',
     revision => 'kibana-ruby',
+    require  => Class['git'],
   }
 
-#  bundler::install { '/srv/www/kibana': require => Vcsrepo['/srv/www/kibana'] }
+  exec { 'bundler':
+    command => "/usr/bin/bundle install",
+    cwd     => $path,
+    require => [ Vcsrepo[$path], Package['bundler'] ],
+  }
 }
 
 # vim: set ts=2 sw=2 et ft=puppet:
