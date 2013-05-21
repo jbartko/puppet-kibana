ad4a89742e9017a6d2801bd2ec19510c2e403b63
John Bartko <jbartko@txstate.edu>
2013-05-20 22:08:35 -0500

    system-ruby mk0

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,22 +2,15 @@ class kibana::install {
   # TODO: parameterize this
   $dir = '/srv/www/kibana'
 
+  class { 'apache': default_vhost => false, }
+  apache::mod { 'passenger': }
+
   # TODO: modulefile requires puppetlabs-ruby
-  #class { 'ruby':
-  #  version         => 'latest',
-  #  gems_version    => 'latest',
-  #  rubygems_update => false,
-  #}
-  include rvm
-  rvm_system_ruby { 'ruby-2.0.0-p0': ensure => present, default_use => true;
-                    'ruby-2.0.0-p0-dev': ensure => present, }
-  rvm_gem { 'bundler':
-    ensure       => present,
-    ruby_version => 'ruby-2.0.0-p0',
-    require      => Rvm_system_ruby['ruby-2.0.0-p0'],
-    before       => Exec['bundler'],
+  class { 'ruby':
+    version         => 'latest',
+    gems_version    => 'latest',
+    rubygems_update => false,
   }
-  rvm::system_user { 'jbartko': }
 
   # TODO: modulefile requires puppetlabs-git
   include git
@@ -36,18 +29,54 @@ class kibana::install {
     path    => $::path,
     cwd     => $dir,
     unless  => 'bundle check',
-    require => [ Vcsrepo[$dir], Rvm_gem['bundler'] ],
-  }
-
-  class { 'apache': default_vhost => false, }
-
-  # TODO: modulefile requires blt04-rvm
-  class { 'rvm::passenger::apache':
-    version      => '4.0.2',
-    ruby_version => 'ruby-2.0.0-p0',
-    mininstances => '3',
-    maxpoolsize  => '30',
+    require => [ Vcsrepo[$dir], Class['bundler'] ],
   }
 }
+
+class bundler {
+  package { 'bundler':
+    ensure   => latest,
+    provider => 'gem',
+    require  => Class['ruby'],
+  }
+}
+# Warning: puppet abuse!
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#  # TODO: modulefile requires blt04-rvm
+#  class { 'rvm::passenger::apache':
+#    version      => '4.0.2',
+#    ruby_version => 'ruby-2.0.0-p0',
+#    mininstances => '3',
+#    maxpoolsize  => '30',
+#  }
+#  include rvm
+#  rvm_system_ruby { 'ruby-2.0.0-p0': ensure => present, default_use => true;
+#                    'ruby-2.0.0-p0-dev': ensure => present, }
+#  rvm_gem { 'bundler':
+#    ensure       => present,
+#    ruby_version => 'ruby-2.0.0-p0',
+#    require      => Rvm_system_ruby['ruby-2.0.0-p0'],
+#    before       => Exec['bundler'],
+#  }
+#  rvm::system_user { 'jbartko': }
 
 # vim: set ts=2 sw=2 et ft=puppet:
