b1a4997360a85727b99837de7380ed9099d67ae0
John Bartko <jbartko@txstate.edu>
2013-05-22 21:39:54 -0500

    yes ruby dev

--- manifests/config.pp
+++ manifests/config.pp
@@ -8,7 +8,7 @@ class kibana::config {
   if $kibana::rvm == true {
     class { 'rvm::passenger::apache':
       version      => '4.0.2',
-      ruby_version => 'ruby-2.0.0-p0',
+      ruby_version => 'ruby-2.0.0-p0-dev',
       mininstances => '3',
       maxpoolsize  => '30',
     }
