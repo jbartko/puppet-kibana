9b425c5236269a3d0f659d6104a1d0dafb802776
John Bartko <jbartko@txstate.edu>
2013-05-22 21:36:31 -0500

    no ruby dev

--- manifests/install.pp
+++ manifests/install.pp
@@ -2,8 +2,7 @@ class kibana::install {
   if $kibana::rvm == true {
     # TODO: modulefile requires blt04-rvm
     include rvm
-    rvm_system_ruby { 'ruby-2.0.0-p0': ensure => present, default_use => true;
-                      'ruby-2.0.0-p0-dev': ensure => present, }
+    rvm_system_ruby { 'ruby-2.0.0-p0': ensure => present, default_use => true; }
     rvm_gem { 'bundler':
       ensure       => present,
       ruby_version => 'ruby-2.0.0-p0',
