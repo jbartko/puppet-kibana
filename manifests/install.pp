ab1c740b9ff36bd140218e68867ab30cb0b651fb
John Bartko <jbartko@txstate.edu>
2013-09-01 17:35:42 -0500

    version bump

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,30 +1,7 @@
 class kibana::install {
-#  include gcc
-#  if $kibana::rvm == true {
-#    include rvm
-#    rvm_system_ruby { 'ruby-2.0.0-p0-dev':
-#      ensure      => present,
-#      default_use => true;
-#    }
-#    rvm_gem { 'bundler':
-#      ensure       => present,
-#      ruby_version => 'ruby-2.0.0-p0-dev',
-#      require      => Rvm_system_ruby['ruby-2.0.0-p0-dev'],
-#      before       => Exec['bundler'],
-#    }
-#  } else {
-#    class { 'ruby':
-#      version         => 'latest',
-#      gems_version    => 'latest',
-#      rubygems_update => false,
-#    }
-#    include ruby::dev
-#    include bundler
-#  }
+  include git
 
   class { 'apache': default_vhost => false, }
-
-  include git
 
   file { $kibana::install_root: ensure => directory }
 }
